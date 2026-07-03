70e0d11e206a800f3948609aac5b8d80f1304049
Iyari14 <joseiyagironn@gmail.com>
2026-07-02 21:18:40 -0600

    Add frame.py to create and save a PNG image

---
+++ animatics/frame.py
@@ -0,0 +1,12 @@
+import cairo
+
+WIDTH = 1920
+HEIGHT = 1080
+
+surface = cairo.ImageSurface(cairo.FORMAT_ARGB32, WIDTH, HEIGHT)
+context = cairo.Context(surface)
+
+context.set_source_rgb(0.98, 0.96, 0.91)
+context.paint()
+
+surface.write_to_png("frame.png")
