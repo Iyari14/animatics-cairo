41cc54c7763fc61ccf4ea2a2b55d8f10bf3e2653
Iyari14 <joseiyagironn@gmail.com>
2026-07-03 22:03:04 -0600

    Add Sim class for simulation with rendering

--- animatics/frame.py
+++ animatics/frame.py
@@ -10,3 +10,15 @@ context.set_source_rgb(0.98, 0.96, 0.91)
 context.paint()
 
 surface.write_to_png("frame.png")
+
+class Sim:
+  def __init__(self, mobjects=[]):
+    self.mobjects = mobjects
+  def run(self):
+    WIDTH = 1920
+    HEIGHT = 1080
+    surface = cairo.ImageSurface(cairo.FORMAT_ARGB32, WIDTH, HEIGHT)
+    context = cairo.Context(surface)
+    context.set_source_rgb(0.98, 0.96, 0.91)
+    context.paint()
+    surface.write_to_png("frame0.png")
